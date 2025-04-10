e655cb44698d58475ca16a49b301ffefd6f67d5c
NIRMAL KANERIA <nk6682728@gmail.com>
2025-04-10 18:45:29 +0530

    First Commit10/04/2025

---
+++ learning.js
@@ -0,0 +1,49 @@
+// const day = prompt("Enter the day name to check the menu");
+
+// switch (day) {
+//   case "monday":
+//     console.log("Poha");
+//     break;
+//   case "tuesday":
+//     console.log("Vedai");
+//     break;
+//   case "wednesday":
+//     console.log("Dosa");
+//     break;
+//   case "thursday":
+//     console.log("parathe");
+//     break;
+//   case "friday":
+//     console.log("Upma");
+//     break;
+//   case "saturday":
+//     console.log("Idli");
+//     break;
+//   case "sunday":
+//     console.log("Sabudana Khichdi");
+//     break;
+//   default:
+//     console.log("Please enter a valid day name (e.g., Monday, Tuesday).");
+// }
+
+const dayName = prompt("enter day");
+
+if (dayName==="monday") {
+  console.log("poha");
+} else if (dayName==='tuesday') {
+  console.log("vedai");
+} else if (dayName==='wednesday') {
+  console.log("dosa");
+} else if (dayName==='thursday') {
+  console.log("parathe");
+} else if (dayName==='friday') {
+  console.log("upma");
+} else if (dayName==='saturday') {
+  console.log("idli");
+} else if (dayName==='sunday') {
+  console.log ("sabudana khichdi");
+} else {
+  console.log("please enter a valid day name (e.g., monday, tuesday).");
+}
+
+
